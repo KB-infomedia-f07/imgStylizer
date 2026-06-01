cbcf1fb52c539f70a48e77ce0060a9e5eccff6de
Hanna-Johansson-KB <Hanna.Johansson@amal.se>
2026-06-01 13:42:02 +0200

    Add project files.

---
+++ imgStylizer/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace imgStylizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Originalimg = new PictureBox();
+            Generatedimg = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)Originalimg).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Generatedimg).BeginInit();
+            SuspendLayout();
+            // 
+            // Originalimg
+            // 
+            Originalimg.Image = (Image)resources.GetObject("Originalimg.Image");
+            Originalimg.Location = new Point(12, 69);
+            Originalimg.Name = "Originalimg";
+            Originalimg.Size = new Size(330, 320);
+            Originalimg.SizeMode = PictureBoxSizeMode.Zoom;
+            Originalimg.TabIndex = 0;
+            Originalimg.TabStop = false;
+            // 
+            // Generatedimg
+            // 
+            Generatedimg.Location = new Point(458, 69);
+            Generatedimg.Name = "Generatedimg";
+            Generatedimg.Size = new Size(330, 320);
+            Generatedimg.SizeMode = PictureBoxSizeMode.Zoom;
+            Generatedimg.TabIndex = 1;
+            Generatedimg.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(351, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Generate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(Generatedimg);
+            Controls.Add(Originalimg);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)Originalimg).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Generatedimg).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox Originalimg;
+        private PictureBox Generatedimg;
+        private Button button1;
+    }
+}
---
+++ imgStylizer/Form1.cs
@@ -0,0 +1,73 @@
+namespace imgStylizer
+{
+    public partial class Form1 : Form
+    {
+
+        List<Color> imgColors;
+        public Form1()
+        {
+            InitializeComponent();
+            imgColors = [Color.FromArgb(255, 255, 255),
+                         Color.FromArgb(177, 52, 37),
+                         Color.FromArgb(227, 157, 37),
+                         Color.FromArgb(106, 107, 4)];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap originalImg = (Bitmap)Originalimg.Image;
+            Bitmap newImg = new Bitmap(originalImg.Width,originalImg.Height);
+            Bitmap bestImg = new Bitmap(originalImg.Width, originalImg.Height);
+
+            Random rand = new Random();
+            for (int j = 0; j < 5; j++)
+            {
+                Bitmap bestGenerationImg = new Bitmap(originalImg.Width, originalImg.Height);
+                long bestScore = long.MaxValue;
+                for (int i = 0; i < 100; i++)
+                {
+                    newImg = (Bitmap)bestImg.Clone();
+                    //generate start point 
+                    int x1 = rand.Next(newImg.Width);
+                    int y1 = rand.Next(newImg.Height);
+                    //generate end point
+                    int x2 = x1 + rand.Next(newImg.Width - x1);
+                    int y2 = y1 + rand.Next(newImg.Height - y1);
+                    //generate color
+                    int colorIndex = rand.Next(imgColors.Count);
+                    var pixelcolor = imgColors[colorIndex];
+                    int r = pixelcolor.R;
+                    int b = pixelcolor.B;
+                    int g = pixelcolor.G;
+                    var newPixel = Color.FromArgb(r, g, b);
+                    //draw rectangle
+                    for (int x = x1; x < x2; x++)
+                    {
+                        for (int y = y1; y < y2; y++)
+                        { 
+                            newImg.SetPixel(x, y, newPixel);
+                        }
+                    }
+                    //score rectangle
+                    long score = 0;
+                    for (int x = 0; x < newImg.Width; x++)
+                    {
+                        for (int y = 0; y < newImg.Height; y++)
+                        {
+                            var origianlPixel = originalImg.GetPixel(x, y);
+                            score += (int)(Math.Pow(Math.Abs(origianlPixel.R - r), 3) + Math.Pow(Math.Abs(origianlPixel.B - b), 3) + Math.Pow(Math.Abs(origianlPixel.G - g), 3));
+                        }
+                    }
+                    if (score < bestScore)
+                    {
+                        bestScore = score;
+                        bestGenerationImg = (Bitmap)newImg.Clone();
+                    }
+                    //MessageBox.Show($"Score: {score}");
+                }
+                bestImg = (Bitmap)bestGenerationImg.Clone();
+            }
+            Generatedimg.Image = bestImg;
+        }
+    }
+}
