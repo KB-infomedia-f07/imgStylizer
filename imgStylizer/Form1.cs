aa9762b15bbeaa4222262ffa602e3a82fe77ae02
Hanna-Johansson-KB <Hanna.Johansson@amal.se>
2026-06-02 12:44:00 +0200

    automatic colorspace v1

--- imgStylizer/Form1.cs
+++ imgStylizer/Form1.cs
@@ -4,13 +4,42 @@ namespace imgStylizer
     {
 
         List<Color> imgColors;
+        List<MyColor> imgMyColors;
         public Form1()
         {
             InitializeComponent();
+            /*
             imgColors = [Color.FromArgb(255, 255, 255),
                          Color.FromArgb(177, 52, 37),
                          Color.FromArgb(227, 157, 37),
                          Color.FromArgb(106, 107, 4)];
+            */
+            imgColors = [];
+            imgMyColors = [];
+            Bitmap originalImg = (Bitmap)Originalimg.Image;
+            for (int x = 0; x < originalImg.Width; x++)
+            {
+                for (int y = 0; y < originalImg.Height; y++)
+                {
+                    imgColors.Add(originalImg.GetPixel(x, y));
+                    imgMyColors.Add(new MyColor(originalImg.GetPixel(x, y)));
+                }
+            }
+            imgMyColors.Sort();
+            List<Color> SortedImgColors = imgColors.OrderBy(color => color.R * 256 * 256 + color.B * 256 + color.G).ToList();
+            foreach (MyColor color in imgMyColors)
+            {
+                //MessageBox.Show($"R: {color.Color.R} B: {color.Color.B} G: {color.Color.G}");
+            }
+
+            Color[,] colors = new Color[10, originalImg.Width * originalImg.Height/10];
+            for(int i = 0; i < colors.GetLength(0); i++)
+            {
+                for (int j = 0; j < colors.GetLength(1); j++)
+                {
+                    colors[i, j] = SortedImgColors[i * colors.GetLength(1) +j];
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,7 +51,7 @@ namespace imgStylizer
             Random rand = new Random();
             for (int j = 0; j < 5; j++)
             {
-                Bitmap bestGenerationImg = new Bitmap(originalImg.Width, originalImg.Height);
+                Bitmap bestGenerationImg = (Bitmap)bestImg.Clone();
                 long bestScore = long.MaxValue;
                 for (int i = 0; i < 100; i++)
                 {
@@ -34,8 +63,7 @@ namespace imgStylizer
                     int x2 = x1 + rand.Next(newImg.Width - x1);
                     int y2 = y1 + rand.Next(newImg.Height - y1);
                     //generate color
-                    int colorIndex = rand.Next(imgColors.Count);
-                    var pixelcolor = imgColors[colorIndex];
+                    var pixelcolor = imgColors[rand.Next(imgColors.Count)];
                     int r = pixelcolor.R;
                     int b = pixelcolor.B;
                     int g = pixelcolor.G;
@@ -55,7 +83,9 @@ namespace imgStylizer
                         for (int y = 0; y < newImg.Height; y++)
                         {
                             var origianlPixel = originalImg.GetPixel(x, y);
-                            score += (int)(Math.Pow(Math.Abs(origianlPixel.R - r), 3) + Math.Pow(Math.Abs(origianlPixel.B - b), 3) + Math.Pow(Math.Abs(origianlPixel.G - g), 3));
+                            score += (long)(Math.Pow(Math.Abs(origianlPixel.R - r), 4) + 
+                                            Math.Pow(Math.Abs(origianlPixel.B - b), 4) + 
+                                            Math.Pow(Math.Abs(origianlPixel.G - g), 4));
                         }
                     }
                     if (score < bestScore)
---
+++ imgStylizer/MyColor.cs
@@ -0,0 +1,27 @@
+﻿class MyColor : IComparable<MyColor>
+{
+    public Color Color { get; set; }
+    public int value { get; }
+
+    public MyColor(Color color)
+    {
+        Color = color;
+        value = Color.R * 256 * 256 + Color.B * 256 + Color.G;
+    }
+    public int CompareTo(MyColor? other)
+    {
+        /*
+        if(value < other.value)
+        {
+            return -1;
+        } else if(value > other.value)
+        {
+            return 1;
+        } else
+        {
+            return 0;
+        }
+        */
+        return this.value.CompareTo(other.value);
+    }
+}
